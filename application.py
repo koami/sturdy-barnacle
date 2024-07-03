f19b31abb520ae3b685c0fed64256173f65af554
koami <van.romy8@gmail.com>
2024-07-03 18:16:07 +0000

    Chebyshev distance using NumPy

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
+import numpy as np
+
 def chebyshev_distance(p1, p2):
-    """Docstring..."""
-    return max(abs(x - y) for x, y in zip(p1, p2))
+    """Calculate Chebyshev distance using NumPy"""
+    p1 = np.array(p1)
+    p2 = np.array(p2)
+    return np.max(np.abs(p1 - p2))
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
