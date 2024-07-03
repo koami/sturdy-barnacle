a5fa71954bba1d7a2b9ad06a6169f18d158e4127
koami <van.romy8@gmail.com>
2024-07-03 18:20:58 +0000

    Chebyshev distance using a for Loop

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
 def chebyshev_distance(p1, p2):
-    """Calculate Chebyshev distance using map"""
-    return max(map(lambda x, y: abs(x - y), p1, p2))
+    """Calculate Chebyshev distance using a for loop."""
+    max_diff = 0
+    for x, y in zip(p1, p2):
+        diff = abs(x - y)
+        if diff > max_diff:
+            max_diff = diff
+    return max_diff
 
 if __name__ == "__main__":
     point1 = (1, 2, 3)
